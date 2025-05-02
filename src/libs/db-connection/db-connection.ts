74972372e0c736dd1952a7498d85647018cd16fc
vindexTOS <giorgikutateladze1998@gmail.com>
2025-05-02 15:32:45 +0400

    dynamic host name

--- src/libs/db-connection/db-connection.ts
+++ src/libs/db-connection/db-connection.ts
@@ -2,7 +2,7 @@ import { createConnection } from "mysql2/promise";
 
 async function ensureDatabase() {
     const connection = await createConnection({
-      host: 'localhost',
+      host: process.env.DB_HOST,
       port: 3306,
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
